6729a3ce2240743c79027a246d9cf175fbea37d7
leilafeiguin <leilafeiguin@gmail.com>
2018-04-11 19:30:52 -0300

    Proceso ESI: levanto archivo de config y logger

--- ESI/src/ESI.c
+++ ESI/src/ESI.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "ESI.h"
+
+void* archivo;
+t_log* logger;
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	char* fileLog;
+	fileLog = "ESI_logs.txt";
+
+	logger = log_create(fileLog, "ESI Logs", 1, 1);
+	log_info(logger, "Inicializando proceso ESI. \n");
+
+	ESI_configuracion configuracion = get_configuracion();
+	log_info(logger, "Archivo de configuracion levantado. \n");
+
+
 	return EXIT_SUCCESS;
+}
+
+ESI_configuracion get_configuracion() {
+	printf("Levantando archivo de configuracion del proceso ESI\n");
+	ESI_configuracion configuracion;
+	t_config* archivo_configuracion = config_create(pathESIConfig);
+	configuracion.IP_COORDINADOR = get_campo_config_string(archivo_configuracion, "IP_FILESYSTEM");
+	configuracion.PUERTO_COORDINADOR = get_campo_config_int(archivo_configuracion, "PUERTO_FILESYSTEM");
+	configuracion.IP_PLANIFICADOR = get_campo_config_string(archivo_configuracion, "NOMBRE_NODO");
+	configuracion.PUERTO_PLANIFICADOR = get_campo_config_int(archivo_configuracion, "IP_NODO");
+	return configuracion;
 }
--- ESI/src/ESI.h
+++ ESI/src/ESI.h
@@ -1,6 +1,19 @@
 #ifndef ESI_H_
 #define ESI_H_
 
+#include <Libraries.h>
+#include <commons/log.h>
 
+typedef struct ESI_configuracion {
+	char* IP_COORDINADOR;
+	int PUERTO_COORDINADOR;
+	char* IP_PLANIFICADOR;
+	int PUERTO_PLANIFICADOR;
+
+} ESI_configuracion;
+
+const char* pathESIConfig = "/home/utnso/workspace/tp-2018-1c-PuntoZip/ESI/configESI.cfg";
+
+ESI_configuracion get_configuracion();
 
 #endif /* ESI_H_ */
