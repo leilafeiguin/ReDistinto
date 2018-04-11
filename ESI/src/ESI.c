da8c7bf8c17fd9916c46a3d8b304165b3ab94aed
marcoporracin <marcoporracin@gmail.com>
2018-04-11 10:02:06 -0300

    Clear comments

--- ESI/src/ESI.c
+++ ESI/src/ESI.c
@@ -1,13 +1,3 @@
-/*
- ============================================================================
- Name        : ESI.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 
