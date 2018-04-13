e0a1bd8758f55fae80c6255d45834dcf1264b824
marcoporracin <marcoporracin@gmail.com>
2018-04-13 12:32:38 -0300

    Conexiones terminadas (se agregaron las de planificador)

--- Planificador/src/Planificador.c
+++ Planificador/src/Planificador.c
@@ -17,6 +17,117 @@ int main(void) {
 
 	iniciarConsolaPlanificador();
 
+	un_socket Coordinador = conectar_a(configuracion.IP_COORDINADOR,configuracion.PUERTO_COORDINADOR);
+	realizar_handshake(Coordinador, cop_handshake_Planificador_Coordinador);
+	int tamanio = 0; //Calcular el tamanio del paquete
+	void* buffer = malloc(tamanio); //Info que necesita enviar al coordinador.
+	enviar(Coordinador,cop_generico,tamanio,buffer);
+	log_info(logger, "Me conecte con el Coordinador. \n");
+
+/*
+--------------------------------------------------------
+----------------- Implementacion Select ----------------
+--------------------------------------------------------
+*/
+
+	FD_ZERO(&master);    // clear the master and temp sets
+	FD_ZERO(&read_fds);
+
+	// get us a socket and bind it
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+	if ((rv = getaddrinfo(NULL, configuracion.PUERTO_ESCUCHA, &hints, &ai)) != 0) {
+		fprintf(stderr, "selectserver: %s\n", gai_strerror(rv));
+		exit(1);
+	}
+
+	for(p = ai; p != NULL; p = p->ai_next) {
+		listener = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+		if (listener < 0) {
+			continue;
+		}
+		// lose the pesky "address already in use" error message
+		setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+		if (bind(listener, p->ai_addr, p->ai_addrlen) < 0) {
+			close(listener);
+			continue;
+		}
+		break;
+	}
+	// if we got here, it means we didn't get bound
+	if (p == NULL) {
+		fprintf(stderr, "selectserver: failed to bind\n");
+		exit(2);
+	}
+	freeaddrinfo(ai); // all done with this
+	// listen
+	if (listen(listener, 10) == -1) {
+		perror("listen");
+		exit(3);
+	}
+	// add the listener to the master set
+	FD_SET(listener, &master);
+	// add the Coordinador to the master set
+	FD_SET(Coordinador, &master);
+	// keep track of the biggest file descriptor
+	fdmax = listener; // so far, it's this one
+	if (Coordinador > fdmax) {    //Update el Maximo
+		fdmax = Coordinador;
+	}
+
+
+/*
+--------------------------------------------------------
+--------------------- Conexiones -----------------------
+--------------------------------------------------------
+*/
+
+	while(1){
+		read_fds = master;
+		select(fdmax+1, &read_fds, NULL, NULL, NULL);
+		int socketActual;
+		for(socketActual = 0; socketActual <= fdmax; socketActual++) {
+			if (FD_ISSET(socketActual, &read_fds)) {
+				if (socketActual == listener) { //es una conexion nueva
+					newfd = aceptar_conexion(socketActual);
+					t_paquete* handshake = recibir(socketActual);
+					FD_SET(newfd, &master); //Agregar al master SET
+					if (newfd > fdmax) {    //Update el Maximo
+						fdmax = newfd;
+					}
+					log_info(logger, "Recibi una nueva conexion. \n");
+					free(handshake);
+				} else { //No es una nueva conexion -> Recibo el paquete
+					t_paquete* paqueteRecibido = recibir(socketActual);
+					switch(paqueteRecibido->codigo_operacion){
+						case cop_handshake_ESI_Planificador:
+
+							esperar_handshake(socketActual,paqueteRecibido,cop_handshake_ESI_Planificador);
+							log_info(logger, "Realize handshake con ESI \n");
+							paqueteRecibido = recibir(socketActual); // Info sobre el ESI
+
+							//Todo actualizar estructuras necesarias con datos del ESI
+
+						break;
+						case cop_Coordinador_Sentencia_Exito:
+
+						break;
+						case cop_Coordinador_Sentencia_Fallo_TC:
+
+						break;
+						case cop_Coordinador_Sentencia_Fallo_CNI:
+
+						break;
+						case cop_Coordinador_Sentencia_Fallo_CI:
+
+						break;
+					}
+				}
+			}
+		}
+	}
 	return EXIT_SUCCESS;
 }
 
--- Planificador/src/Planificador.h
+++ Planificador/src/Planificador.h
@@ -24,4 +24,28 @@ void iniciarConsolaPlanificador();
 
 char** validaCantParametrosComando(char* comando, int cantParametros);
 
+/*
+--------------------------------------------------------
+----------------- Variables para el SV -----------------
+--------------------------------------------------------
+*/
+fd_set master;    // master file descriptor list
+fd_set read_fds;  // temp file descriptor list for select()
+int fdmax;        // maximum file descriptor number
+int listener;     // listening socket descriptor
+int newfd;        // newly accept()ed socket descriptor
+struct sockaddr_storage remoteaddr; // client address
+socklen_t addrlen;
+char buf[256];    // buffer for client data
+int nbytes;
+char remoteIP[INET6_ADDRSTRLEN];
+int yes=1;        // for setsockopt() SO_REUSEADDR, below
+int i, j, rv;
+struct addrinfo hints, *ai, *p;
+/*
+--------------------------------------------------------
+----------------- FIN Variables para el SV -------------
+--------------------------------------------------------
+*/
+
 #endif /* PLANIFICADOR_H_ */
